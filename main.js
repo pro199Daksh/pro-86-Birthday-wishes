a87f372a8b84978cd7fdefbe885bc557c830c707
pro199Daksh <88223631+pro199Daksh@users.noreply.github.com>
2022-07-30 11:49:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+ image_object=""
+
+function new_image()
+{
+	fabric.Image.fromURL("BirthdayImage.jpg", function(Img){
+        image_object= Img;
+        image_object.scaleToWidth(700)
+        image_object.scaleToHeight(500)
+
+        image_object.set({
+            top:0,
+            left:0
+        })
+        canvas.add(image_object);	
+
+    })	
+}
+
+function playSound(){
+	x.play()
+}
